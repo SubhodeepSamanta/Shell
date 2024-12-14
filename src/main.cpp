72267920e7aa1c017561219cb943ef5a077d6f63
SubhodeepSamanta <subhodeepsamanta2005@gmail.com>
2024-12-14 17:23:25 +0530

    exit command

--- src/main.cpp
+++ src/main.cpp
@@ -5,11 +5,13 @@ int main() {
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
-  // Uncomment this block to pass the first stage
-  while(true){
   std::cout << "$ ";
   std::string input;
   std::getline(std::cin, input);
+
+  while(input!="exit"){
+  std::getline(std::cin, input);
   std::cout << input << ": command not found\n";
   }
+  return 0;
 }
